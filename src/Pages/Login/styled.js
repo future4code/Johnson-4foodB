aa615d0aaaa0142f739c6e7267ae7c974a2191eb
RubensSantos00 <rubenscvsantos@gmail.com>
2021-10-13 20:25:55 -0300

    Inicio da página de cadastro

--- src/Pages/Login/styled.js
+++ src/Pages/Login/styled.js
@@ -1,17 +1,5 @@
 import styled from "styled-components";
 
-export const ContainerPage = styled.div`
-  min-width: 400px;
-  max-width: 900px;
-  height: 100vh;
-  background-color: #e8222e;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  font-family: sans-serif;
-`;
 
 export const Wrapper = styled.div`
   height: 100vh;
